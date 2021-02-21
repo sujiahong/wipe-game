f97b989f3c3cbb111d8d88478c23a176a25646a5
sujiahong <1511877962@qq.com>
2021-02-21 16:56:51 +0800

    新提交图片资源

---
+++ tools/toLevelJson.js
@@ -0,0 +1,27 @@
+const xlsx = require("node-xlsx");
+const fs = require("fs");
+var level = {};
+var list = xlsx.parse("level.xlsx")[0].data;
+console.log(list)
+
+// var keyItem = list[1];
+// var typeItem = list[2];
+// for (var i = 3; i < list.length; ++i){
+//     var item = list[i];
+//     var temp = {};
+//     for(var j = 0; j < keyItem.length; ++j){
+//         if (typeItem[j] == "arrayint"){
+//             var arr = item[j].split(";");
+//             if (arr[arr.length - 1] == "")
+//                 arr.pop();
+//             temp[keyItem[j]] = arr;
+//         }else{
+//             temp[keyItem[j]] = item[j];
+//         }
+//     }
+//     level[item[0]] = temp;
+// }
+
+console.log(level);
+
+fs.writeFileSync("../assets/resources/config/level.json", JSON.stringify(level));
